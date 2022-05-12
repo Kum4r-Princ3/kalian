c6623b4052e6c22d0b86daedb39fb4bf58badc50
Kum4r_Princ3 <57240372+Kum4r-Princ3@users.noreply.github.com>
2022-05-12 05:00:33 +0000

    Update kalian.sh

--- kalian.sh
+++ kalian.sh
@@ -12,7 +12,16 @@ printf "
 
 <<-----------------------------------------------(by Prince Kumar )-------------------------------------------------->>
 "
-
+# Init
+FILE="/tmp/out.$$"
+GREP="/bin/grep"
+#....
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+# ...
 curl -L https://archive.kali.org/archive-key.asc | apt-key add
 echo "deb http://kali.download/kali kali-rolling main contrib non-free" | sudo tee /etc/apt/sources.list
 sleep 2
